40b7440678cf38d8cc02cccbdcf6885db20ac4b3
d9553065 <d9553065@fcu.edu.tw>
2025-03-27 18:03:06 +0800

    商品功能 - 實作查詢商品

--- src/main/java/com/hobart/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/hobart/springbootmall/dao/impl/ProductDaoImpl.java
@@ -2,10 +2,22 @@ package com.hobart.springbootmall.dao.impl;
 
 import com.hobart.springbootmall.dao.ProductDao;
 import com.hobart.springbootmall.model.Product;
+import com.hobart.springbootmall.rowmapper.ProductRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class ProductDaoImpl implements ProductDao {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public ProductDaoImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
 
     @Override
     public Product getProductById(Integer productId) {
@@ -14,6 +26,16 @@ public class ProductDaoImpl implements ProductDao {
                 "created_date, last_modified_date " +
                 "FROM product WHERE product_id = :product_id";
 
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        map.put("product_id", productId);
+
+        List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
+
+        if (!productList.isEmpty()) {
+            return productList.getFirst();
+        } else {
+            return null;
+        }
+
     }
 }
---
+++ src/main/java/com/hobart/springbootmall/service/impl/ProductServiceImpl.java
@@ -0,0 +1,19 @@
+package com.hobart.springbootmall.service.impl;
+
+
+import com.hobart.springbootmall.dao.ProductDao;
+import com.hobart.springbootmall.model.Product;
+import com.hobart.springbootmall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Override
+    public Product getProductById(Integer productId) {
+        return productDao.getProductById(productId);
+    }
+}
