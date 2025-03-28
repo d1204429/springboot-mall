5922f0a0d59918edcb3554f779b52e6ce741e62c
d9553065 <d9553065@fcu.edu.tw>
2025-03-28 17:00:11 +0800

    商品功能 - 實作查詢商品列表

--- src/main/java/com/hobart/springbootmall/dao/ProductDao.java
+++ src/main/java/com/hobart/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.hobart.springbootmall.dao;
 import com.hobart.springbootmall.dto.ProductRequest;
 import com.hobart.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById (Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- src/main/java/com/hobart/springbootmall/service/ProductService.java
+++ src/main/java/com/hobart/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.hobart.springbootmall.service;
 import com.hobart.springbootmall.dto.ProductRequest;
 import com.hobart.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById (Integer productId);
 
